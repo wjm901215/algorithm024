baafc3587e3e0796b97fa845401c2e7ff0daa7d0
wangjm <wangjm@zhiyuaniot.com>
2021-02-08 15:39:22 +0800

    week2 home work update

--- Week_02/丑数_.java
+++ Week_02/丑数_.java
@@ -2,6 +2,40 @@ public class NthUglyNumber {
     private int[] uglyNumber = {2,3,5};
 
     /**
+     * 解法1
+     * 通过遍历，循环判断丑数，直到集合大小到达给定的n值
+     * @param n
+     * @return
+     */
+    public int nthUglyNumber(int n) {
+        List<Integer> res = new ArrayList();
+        int i = 1;
+        while  (res.size() < n){
+            if (isUgly(i)) {
+                res.add(i);
+            }
+            i++;
+        }
+        return res.get(n-1);
+    }
+    /**
+     * 判断是否为丑数
+     * 首先除2，直到不能整除为止，然后除3到不能整除为止，然后除5直到不能整除为止。最终判断剩余的数字是否为1，如果是1则为丑数，否则不是丑数
+     */
+    private static boolean isUgly(int num) {
+        while (num % 2 == 0) {
+            num /= 2;
+        }
+        while (num % 3 == 0) {
+            num /= 5;
+        }
+        while (num % 5 == 0) {
+            num /= 5;
+        }
+        return num == 1;
+    }
+    /**
+     * 解法2
      * 利用小根堆，然后1作为第一个丑数，每次从小根堆弹出最小的丑数，
      * 然后记录已弹出丑数的个数，如果count>=n,返回当前弹出的元素，否则继续乘以2、3、5
      * @param n
